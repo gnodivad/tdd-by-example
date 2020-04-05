bc224c61b1e7e74e5b50152544e7758c509e72fd
David Ong <dodwei.dev@gmail.com>
2020-04-05 10:45:36 +0800

    Finish Chapter 10

---
+++ src/chapter10/Money.ts
@@ -0,0 +1,37 @@
+export class Money {
+    protected amount: number = 0;
+    protected currency: string = "";
+
+    constructor(amount: number, currency: string) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    getCurrency(): string {
+        return this.currency;
+    }
+
+    static dollar(number: number): Money {
+        return new Dollar(number, "USD");
+    }
+
+    static franc(number: number): Money {
+        return new Franc(number, "CHF");
+    }
+
+    equals(object: Object | null): boolean {
+        if (!object) {
+            return false;
+        }
+
+        const money = object as Money;
+        return this.amount == money.amount && this.getCurrency() == money.getCurrency();
+    }
+
+    times(multiplier: number): Money {
+        return new Money(this.amount * multiplier, this.getCurrency());
+    }
+}
+
+import { Dollar } from "./Dollar";
+import { Franc } from "./Franc";
---
+++ src/chapter10/test.ts
@@ -0,0 +1,35 @@
+import { Franc } from "./Franc";
+import { Money } from "./Money";
+
+describe("Dollar", () => {
+    it('multiplication', () => {
+        const five = Money.dollar(5);
+        expect(Money.dollar(10).equals(five.times(2))).toBe(true);
+        expect(Money.dollar(15).equals(five.times(3))).toBe(true);
+    });
+})
+
+describe("Franc", () => {
+    it('multiplication', () => {
+        const five = Money.franc(5);
+        expect(Money.franc(10).equals(five.times(2))).toBe(true);
+        expect(Money.franc(15).equals(five.times(3))).toBe(true);
+    });
+})
+
+it('equality', () => {
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
+    expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
+    expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
+    expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
+});
+
+it("different class equality", () => {
+    expect(new Money(10, "CHF").equals(new Franc(10, "CHF"))).toBe(true);
+})
+
+it('currency', () => {
+    expect(Money.dollar(1).getCurrency()).toEqual("USD");
+    expect(Money.franc(1).getCurrency()).toEqual("CHF");
+})
