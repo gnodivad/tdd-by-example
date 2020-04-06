b6f91abd18304138098c7d465f8c5051cc1781ee
David Ong <dodwei.dev@gmail.com>
2020-04-06 17:35:00 +0800

    Finish Chapter 11

---
+++ src/chapter11/test.ts
@@ -0,0 +1,18 @@
+import { Money } from "./Money";
+
+it('multiplication', () => {
+    const five = Money.dollar(5);
+    expect(Money.dollar(10).equals(five.times(2))).toBe(true);
+    expect(Money.dollar(15).equals(five.times(3))).toBe(true);
+});
+
+it('equality', () => {
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
+    expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
+});
+
+it('currency', () => {
+    expect(Money.dollar(1).getCurrency()).toEqual("USD");
+    expect(Money.franc(1).getCurrency()).toEqual("CHF");
+})
